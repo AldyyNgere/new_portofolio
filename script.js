746c629a7a30740b6f03bf93f2b6720319381839
aldy ngere <aldyngere20@gmail.com>
2023-07-09 21:48:38 -0700

    menyelesaikan design header

---
+++ script.js
@@ -0,0 +1,21 @@
+// Navbar Function
+function navbarFunction() {
+  const navbar = document.querySelector(".navbar-menu");
+  const menuBtn = document.getElementById("menu-btn");
+  const closeBtn = document.getElementById("close-btn");
+
+  menuBtn.addEventListener("click", () => {
+    navbar.classList.add("nav-active");
+    menuBtn.classList.add("nav-active");
+    closeBtn.classList.add("nav-active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    navbar.classList.remove("nav-active");
+    menuBtn.classList.remove("nav-active");
+    closeBtn.classList.remove("nav-active");
+  });
+}
+
+// Call apps
+navbarFunction();
